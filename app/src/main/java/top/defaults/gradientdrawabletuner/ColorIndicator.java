40e2ba150167e8b64a61288f6030254e65cc1bbf
段弘 <duanhong169@gmail.com>
2018-07-16 14:54:14 +0800

    Use ColorPicker from @duanhong169

--- app/src/main/java/top/defaults/gradientdrawabletuner/ColorIndicator.java
+++ app/src/main/java/top/defaults/gradientdrawabletuner/ColorIndicator.java
@@ -1,9 +1,14 @@
 package top.defaults.gradientdrawabletuner;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
+
+import top.defaults.view.ColorPickerPopup;
 
 public class ColorIndicator extends View {
 
@@ -17,7 +22,27 @@ public class ColorIndicator extends View {
 
     public ColorIndicator(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        setOnClickListener(v -> {
+            Drawable background = getBackground();
+            int currentColor = Color.WHITE;
+            if (background instanceof ColorDrawable) {
+                currentColor = ((ColorDrawable) background).getColor();
+            }
+
+            ColorPickerPopup popup = new ColorPickerPopup.Builder(context)
+                    .initialColor(currentColor)
+                    .build();
+            popup.show(v, new ColorPickerPopup.ColorPickerObserver() {
+                @Override
+                public void onColorPicked(int color) {
+                    v.setBackgroundColor(color);
+                }
+
+                @Override
+                public void onColor(int color, boolean fromUser) {
+
+                }
+            });
+        });
     }
-
-
 }
