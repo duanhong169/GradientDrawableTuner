3e8d366bc8dfb3c999ea3faf0f803fc2efc0809a
段弘 <duanhong169@gmail.com>
2018-07-24 15:08:08 +0800

    Extract strings

--- app/src/main/java/top/defaults/gradientdrawabletuner/MainActivity.java
+++ app/src/main/java/top/defaults/gradientdrawabletuner/MainActivity.java
@@ -32,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setTitle("Crafting <shape>");
+        setTitle(getString(R.string.crafting_shape));
 
         viewModel = ViewModelProviders.of(this).get(GradientDrawableViewModel.class);
         ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
