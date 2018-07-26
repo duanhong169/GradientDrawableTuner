1016daeff657688f81173269417c968635cd7330
段弘 <duanhong169@gmail.com>
2018-07-26 16:38:16 +0800

    Enable up button for code activity

--- app/src/main/java/top/defaults/gradientdrawabletuner/XmlCodeViewActivity.java
+++ app/src/main/java/top/defaults/gradientdrawabletuner/XmlCodeViewActivity.java
@@ -3,7 +3,9 @@ package top.defaults.gradientdrawabletuner;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 import butterknife.BindView;
@@ -22,11 +24,27 @@ public class XmlCodeViewActivity extends AppCompatActivity {
         setContentView(R.layout.activity_xml_code_view);
         ButterKnife.bind(this);
 
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setHomeButtonEnabled(true);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
         String code = getIntent().getStringExtra(EXTRA_CODE);
         xmlCodeTextView.setText(code);
         Typeface typeface = Fonts.getDefault(this);
         if (typeface != null) {
             xmlCodeTextView.setTypeface(typeface);
         }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
